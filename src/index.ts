2e2a210c9c0b81f035459123b4e8368630624c0d
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-07-05 12:03:54 -0300

    dynamic router

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+//Routes
+import { router } from './routes';
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cors());
+app.use(router);
+app.listen(PORT, () => console.log(`🚀Server running on port ${PORT}`));
