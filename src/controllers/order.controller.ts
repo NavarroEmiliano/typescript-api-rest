fd6c2d48352d8e5d6a5bd4fbb6c7c0c8fee06cab
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-07-05 19:17:09 -0300

    login user

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,15 +1,16 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import { handlerHttp } from '../utils/error.handler';
+import { RequestExt } from '../middleware/session';
 
-
-const getOrdersController = async (req: Request, res: Response) => {
+const getOrdersController = async (req: RequestExt, res: Response) => {
   try {
-    res.send('This information is only seen by people who are logged in / jwt');
+    res.send({
+      data: 'This information is only seen by people who are logged in / jwt',
+      user: req.user,
+    });
   } catch (error) {
     handlerHttp(res, error as Error);
   }
 };
 
-export {
-  getOrdersController,
-};
+export { getOrdersController };
--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -1,14 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
 import { verifyToken } from '../utils/jwt.handler';
+import { JwtPayload } from 'jsonwebtoken';
 
-const checkJwt = (req: Request, res: Response, next: NextFunction) => {
+export interface RequestExt extends Request {
+  user?: string | JwtPayload;
+}
+
+const checkJwt = (req: RequestExt, res: Response, next: NextFunction) => {
   try {
     const jwtByUser = req.headers.authorization ?? '';
     const jwt = jwtByUser.split(' ').pop();
-    const isValid = verifyToken(`${jwt}`);
-    if (!isValid) {
+    const isUser = verifyToken(`${jwt}`);
+    if (!isUser) {
       return res.status(401).send('Jwt no valid');
     }
+    req.user = isUser;
     next();
   } catch (error) {
     res.status(400).send('Not valid session');
