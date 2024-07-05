41657590d6bb7917404217b0a2cc5ab814a9769f
Emiliano Navarro <emilianonavarro1993@gmail.com>
2024-07-05 12:17:50 -0300

    db conection

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-
-//Routes
 import { router } from './routes';
+import db from './config/mongo';
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use(cors());
 app.use(router);
+db().then(()=> console.log('👌Connection to the database successful'));
 app.listen(PORT, () => console.log(`🚀Server running on port ${PORT}`));
